73295e14d7d296034d4b029a495ff1a1f8302e41
Vladimir Iacob <vladimir.iacob@everymatrix.com>
2017-07-22 10:20:33 +0300

    clean up the repo of old configs bleah

--- wattage.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-bat0=$(awk '{print $1}' /sys/class/power_supply/BAT0/power_now)
-bat1=$(awk '{print $1}' /sys/class/power_supply/BAT1/power_now)
-fake=$((bat0+bat1))
-sum=$(echo $fake|awk '{print $1*10^-6}')
-printf " %0.1fW" "$sum"
